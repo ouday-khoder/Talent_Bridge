c80523576d74963da4f7e85bb2e599706f061b7b
ouday-khoder <147618158+ouday-khoder@users.noreply.github.com>
2024-11-01 10:40:26 +0200

    Add backend structure for freelancer

---
+++ talent-bridge-backend/src/main/java/com/ouday/talent_bridge_backend/repository/FreelancerRepository.java
@@ -0,0 +1,9 @@
+package com.ouday.talent_bridge_backend.repository;
+
+import com.ouday.talent_bridge_backend.entity.Freelancer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FreelancerRepository extends JpaRepository<Freelancer, Integer> {
+}
---
+++ talent-bridge-backend/src/main/java/com/ouday/talent_bridge_backend/service/FreelancerServiceImpl.java
@@ -0,0 +1,8 @@
+package com.ouday.talent_bridge_backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FreelancerServiceImpl implements FreelancerServiceInterface {
+
+}
